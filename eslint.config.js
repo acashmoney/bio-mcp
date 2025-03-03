89806f6f563304c2d1987a5fbc15ff54e980c525
Aakaash Meduri <aakaash.meduri@gmail.com>
2025-03-03 12:47:04 -0500

    lint config

---
+++ eslint.config.js
@@ -0,0 +1,30 @@
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+
+export default [
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+    },
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+    },
+  },
+  // Configuration for non-TypeScript files (optional)
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      sourceType: 'module',
+    },
+  },
+]; 
